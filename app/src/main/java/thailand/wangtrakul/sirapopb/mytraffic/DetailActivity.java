f96adf74544283238680e229c751870b1ad9933c
TonyWang <me_champ1@hotmail.com>
2015-12-27 14:24:20 +0700

    Show View Finish.

--- app/src/main/java/thailand/wangtrakul/sirapopb/mytraffic/DetailActivity.java
+++ app/src/main/java/thailand/wangtrakul/sirapopb/mytraffic/DetailActivity.java
@@ -2,12 +2,59 @@ package thailand.wangtrakul.sirapopb.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    // Explicit.
+    private ImageView imageTraffic;
+    private TextView DetailList;
+    private TextView MainList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // Binding Widget.
+        initWidget();
+        // Show View.
+        showView();
+
+    }// Main Method.
+
+    public void clickBack(View view) {
+        finish();
     }
-}
+
+
+    private void showView() {
+
+        //  Receive Value From Intent.
+        String strTitle = getIntent().getStringExtra("Title");
+        int intImage = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+
+        // Show Title.
+        MainList.setText(strTitle);
+
+        // Show Image.
+        imageTraffic.setImageResource(intImage);
+
+        // Show Detail list.
+        String[] detailString = getResources().getStringArray(R.array.detail_long);
+        DetailList.setText(detailString[intIndex]);
+
+
+    } // Show View Data.
+
+    private void initWidget() {
+        MainList = (TextView) findViewById(R.id.textView4);
+        DetailList = (TextView) findViewById(R.id.textView5);
+        imageTraffic = (ImageView) findViewById(R.id.imageView2);
+    } // Initial Method Widget.
+
+} // Main Detail Activity.
--- app/src/main/java/thailand/wangtrakul/sirapopb/mytraffic/MainActivity.java
+++ app/src/main/java/thailand/wangtrakul/sirapopb/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -32,13 +33,13 @@ public class MainActivity extends AppCompatActivity {
     private void listViewController() {
 
         // #First.
-        int[] intIcon = {   R.drawable.traffic_01,R.drawable.traffic_02,R.drawable.traffic_03,R.drawable.traffic_04,
+        final int[] intIcon = {   R.drawable.traffic_01,R.drawable.traffic_02,R.drawable.traffic_03,R.drawable.traffic_04,
                             R.drawable.traffic_05,R.drawable.traffic_06,R.drawable.traffic_07,R.drawable.traffic_08,
                             R.drawable.traffic_09,R.drawable.traffic_10,R.drawable.traffic_11,R.drawable.traffic_12,
                             R.drawable.traffic_13,R.drawable.traffic_14,R.drawable.traffic_15,R.drawable.traffic_16,
                             R.drawable.traffic_17,R.drawable.traffic_18,R.drawable.traffic_19,R.drawable.traffic_20 };
         // #Second.
-        String[] strTitle = new String[20];
+        final String[] strTitle = new String[20];
         strTitle[0] = "หัวข้อ 1";
         strTitle[1] = "หัวข้อ 2";
         strTitle[2] = "หัวข้อ 3";
@@ -66,6 +67,24 @@ public class MainActivity extends AppCompatActivity {
         // Call MyAdaptor.
         MyAdaptor objMyAdaptor = new MyAdaptor(MainActivity.this, strTitle, strDetail, intIcon);
         trafficListView.setAdapter(objMyAdaptor);
+
+        // Active When Click On ListView.
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // Intent to DetailActivity.
+                Intent objIntent = new Intent(MainActivity.this,DetailActivity.class);
+
+                // Put Extra.
+                objIntent.putExtra("Title", strTitle[position]);
+                objIntent.putExtra("Image", intIcon[position]);
+                objIntent.putExtra("Index", position);
+
+                startActivity(objIntent);
+
+            } // Event.
+        });
 
     }// List View Controller.
 
