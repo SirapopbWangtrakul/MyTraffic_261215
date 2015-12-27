3282898fab2e0fbfbd40eafe619d1e6b43dd3550
TonyWang <me_champ1@hotmail.com>
2015-12-27 11:57:45 +0700

    before designed UI.

--- app/src/main/java/thailand/wangtrakul/sirapopb/mytraffic/MainActivity.java
+++ app/src/main/java/thailand/wangtrakul/sirapopb/mytraffic/MainActivity.java
@@ -20,12 +20,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Bind Widget.
+        // Bind Widget.
         bindWidget();
         // Button Controller.
         bottonController();
+        // ListView Controller.
+        listViewController();
 
     } // Main Method.
+
+    private void listViewController() {
+
+        // #First.
+        int[] intIcon = {   R.drawable.traffic_01,R.drawable.traffic_02,R.drawable.traffic_03,R.drawable.traffic_04,
+                            R.drawable.traffic_05,R.drawable.traffic_06,R.drawable.traffic_07,R.drawable.traffic_08,
+                            R.drawable.traffic_09,R.drawable.traffic_10,R.drawable.traffic_11,R.drawable.traffic_12,
+                            R.drawable.traffic_13,R.drawable.traffic_14,R.drawable.traffic_15,R.drawable.traffic_16,
+                            R.drawable.traffic_17,R.drawable.traffic_18,R.drawable.traffic_19,R.drawable.traffic_20 };
+        // #Second.
+        String[] strTitle = new String[20];
+        strTitle[0] = "หัวข้อ 1";
+        strTitle[1] = "หัวข้อ 2";
+        strTitle[2] = "หัวข้อ 3";
+        strTitle[3] = "หัวข้อ 4";
+        strTitle[4] = "หัวข้อ 5";
+        strTitle[5] = "หัวข้อ 6";
+        strTitle[6] = "หัวข้อ 7";
+        strTitle[7] = "หัวข้อ 8";
+        strTitle[8] = "หัวข้อ 9";
+        strTitle[9] = "หัวข้อ 10";
+        strTitle[10] = "หัวข้อ 11";
+        strTitle[11] = "หัวข้อ 12";
+        strTitle[12] = "หัวข้อ 13";
+        strTitle[13] = "หัวข้อ 14";
+        strTitle[14] = "หัวข้อ 15";
+        strTitle[15] = "หัวข้อ 16";
+        strTitle[16] = "หัวข้อ 17";
+        strTitle[17] = "หัวข้อ 18";
+        strTitle[18] = "หัวข้อ 19";
+        strTitle[19] = "หัวข้อ 20";
+
+        // #Third
+        String[] strDetail = getResources().getStringArray(R.array.detail_short);
+
+        // Call MyAdaptor.
+        MyAdaptor objMyAdaptor = new MyAdaptor(MainActivity.this, strTitle, strDetail, intIcon);
+        trafficListView.setAdapter(objMyAdaptor);
+
+    }// List View Controller.
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
---
+++ app/src/main/java/thailand/wangtrakul/sirapopb/mytraffic/MyAdaptor.java
@@ -0,0 +1,62 @@
+package thailand.wangtrakul.sirapopb.mytraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by TonyWang on 12/27/2015 AD.
+ */
+public class MyAdaptor extends BaseAdapter{
+
+    //Explicit.
+    private Context objContext;
+    private String[] titleStrings, detailStrings;
+    private int[] iconInts;
+
+    public MyAdaptor(Context objContext, String[] titleStrings, String[] detailStrings, int[] iconInts) {
+        this.objContext = objContext;
+        this.titleStrings = titleStrings;
+        this.detailStrings = detailStrings;
+        this.iconInts = iconInts;
+    } // Constructor.
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView = objLayoutInflater.inflate(R.layout.my_listview,parent,false);
+
+        // For TextView.
+        TextView titleTextView = (TextView) objView.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[position]);
+
+        TextView detailTextView = (TextView) objView.findViewById(R.id.textView3);
+        detailTextView.setText(detailStrings[position]);
+
+        // For Icon.
+        ImageView iconImageView = (ImageView) objView.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[position]);
+
+        return objView;
+    }
+} // Main Class.
